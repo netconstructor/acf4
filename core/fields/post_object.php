27d82a706ba09993ae7825e1572a8d9973525858
Elliot Condon <e@elliotcondon.com>
2013-03-28 10:10:53 +1100

    Removed format_value error on the post_object field
    Signed-off-by: Elliot Condon <e@elliotcondon.com>

--- core/fields/post_object.php
+++ core/fields/post_object.php
@@ -322,30 +322,6 @@ class acf_post_object extends acf_field
 	
 	
 	/*
-	*  format_value()
-	*
-	*  This filter is appied to the $value after it is loaded from the db and before it is passed to the create_field action
-	*
-	*  @type	filter
-	*  @since	3.6
-	*  @date	23/01/13
-	*
-	*  @param	$value	- the value which was loaded from the database
-	*  @param	$post_id - the $post_id from which the value was loaded
-	*  @param	$field	- the field array holding all the field options
-	*
-	*  @return	$value	- the modified value
-	*/
-	
-	function format_value( $value, $post_id, $field )
-	{
-		$value = htmlspecialchars($value, ENT_QUOTES);
-		
-		return $value;
-	}
-	
-	
-	/*
 	*  format_value_for_api()
 	*
 	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
